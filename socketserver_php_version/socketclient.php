fab62d93e000a5a1e3f0b277609e8bd0a27d0da2
xiaojielade <xiaojielade@sina.cn>
2016-08-10 19:07:02 +0800

    socket

---
+++ socketserver_php_version/socketclient.php
@@ -0,0 +1,54 @@
+﻿<?php
+$p=fopen("../../www/wangzhan/038/clientip.txt","r+");
+$clientip=fread($p,1024);
+fclose($p);
+file_put_contents("../../www/wangzhan/038/clientip.txt","");
+$client=explode(" ",$clientip);
+$ip=$client[0];
+$port=$client[1];
+$formerpart="tcp://".$ip.":".$port;
+echo $formerpart;
+error_reporting(E_ALL);
+set_time_limit(0);
+echo "<h2>TCP/IP Connection</h2>\n";
+$trans=array();
+$trans[0]="AABB1233FF";
+$trans[1]="AABB1334.5FF";
+$trans[2]="AABB237.6FF";
+$trans[3]="AABB225.3FF";
+$trans[4]="AABB3122FF";
+$trans[5]="AABB3225FF";
+$trans[6]="AABB1240FF";
+$trans[7]="AABB1339FF";
+$trans[8]="AABB214.9FF";
+$trans[9]="AABB235.5FF";
+$trans[10]="AABB3123FF";
+$trans[11]="AABB3235FF";
+while(true){
+	
+	echo "next round";
+	$socket=stream_socket_client($formerpart,$errno,$errstr);
+	//$socket=stream_socket_client("tcp://192.168.1.100:2000",$errno,$errstr);
+	$num=mt_rand(0,10);
+$in=$trans[$num];
+$out = '';
+
+if(!fwrite($socket, $in, strlen($in))) {
+    echo "socket_write() failed: reason: " . socket_strerror($socket) . "\n";
+}else {
+    echo "发到服务器成功了\n";
+    echo "发送的内容为:<font color='red'>$in</font> <br>";
+}
+
+while($out = fread($socket, 8192)) {
+    echo "接收服务器回传信息成功！\n";
+    echo "接受的内容为:$out";
+}
+fclose($socket);
+echo "close socket";
+sleep(1);
+echo "circle end";
+}
+
+
+?>
---
+++ socketserver_php_version/socketserver.php
@@ -0,0 +1,61 @@
+﻿<?php
+
+error_reporting(E_ALL);
+$lianjie=mysql_connect("localhost","root","root");
+mysql_select_db("data",$lianjie);
+//确保在连接客户端时不会超时
+set_time_limit(0);
+
+$ip = '127.0.0.1';
+$port = 2000;
+
+
+if(($sock = socket_create(AF_INET,SOCK_STREAM,SOL_TCP)) < 0) {
+    echo "socket_create() 失败的原因是:".socket_strerror($sock)."\n";
+}
+if(($ret = socket_bind($sock,$ip,$port)) < 0) {
+    echo "socket_bind() 失败的原因是:".socket_strerror($ret)."\n";
+}
+
+if(($ret = socket_listen($sock,4)) < 0) {
+    echo "socket_listen() 失败的原因是:".socket_strerror($ret)."\n";
+}
+
+$count = 0;
+
+do {
+    if (($msgsock = socket_accept($sock)) < 0) {
+        echo "socket_accept() failed: reason: " . socket_strerror($msgsock) . "\n";
+        break;
+    } else {
+        
+        //发到客户端
+        $msg ="测试成功个锤子！\n";
+        socket_write($msgsock, $msg, strlen($msg));
+        
+        echo "测试成功\n";
+		echo "发送的内容是:$msg\n";
+        $buf = socket_read($msgsock,8192);
+        $jg1=array();
+		$jg1=explode("b",$buf,3);
+		foreach($jg1 as $zj){
+			$jg=array();
+			$jg=explode("a",$zj,3);
+			$month=date('Y-m');
+		    $date=date('m-d');
+		    $time=date('Y-m-d H:i:s');
+			$sql="insert into $jg[0] values('$jg[1]','$jg[2]','$month','$date','$time')";
+		    mysql_query($sql);
+			}
+		
+        $talkback = "收到的信息:$buf\n";
+        echo $talkback;
+	
+    }
+    //echo $buf;
+    socket_close($msgsock);
+
+} while (true);
+
+socket_close($sock);
+?>
